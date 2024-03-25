aa6cad95652bb9e1a3e34ea545a3668fdcd66717
Jason Little <realtyem@gmail.com>
2024-03-24 21:08:25 -0500

    Add a bunch of new Exception types that may be used

--- federationbot/errors.py
+++ federationbot/errors.py
@@ -9,3 +9,45 @@ class ServerUnreachable(Exception):
     When the server was offline last time we checked, and we aren't trying them again
     for a while.
     """
+
+
+class ServerDiscoveryError(Exception):
+    """
+    Error occurred during server discovery process
+    """
+
+
+class WellKnownError(ServerDiscoveryError):
+    """
+    Error occurred during the well-known phase of server discovery
+    """
+
+
+class WellKnownHasSchemeError(WellKnownError):
+    """
+    The Host found in well-known has a scheme when it should not
+    """
+
+
+class WellKnownParsingError(WellKnownError):
+    """
+    Error occurred while parsing the well-known response
+    """
+
+
+class MatrixError(Exception):
+    """
+    Generic Matrix-related error
+    """
+
+
+class MNotFound(MatrixError):
+    """
+    The homeserver returned a 404 M_NOT_FOUND
+    """
+
+
+class MForbidden(MatrixError):
+    """
+    The homeserver returned a 403 M_FORBIDDEN
+    """
