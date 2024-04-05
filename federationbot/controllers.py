57017c3f11a71d3a58e503a59a1b2c74311e47a0
Jason Little <realtyem@gmail.com>
2024-04-05 03:45:15 -0500

    Docstrings for ReactionTaskController and children

--- federationbot/controllers.py
+++ federationbot/controllers.py
@@ -26,10 +26,16 @@ class ReactionCommandStatus(Enum):
 
 class ReactionControlEntry:
     """
+    The object for adding control reactions to messages made by the bot. Adds 'Start', 'Stop', and 'Pause'.
+    Intended for use on long running commands that update the message with fresh information.
+
+    This is keyed by the EventID in ReactionTaskController, so it doesn't know what Event it is on. Used by the
+    Reaction handler to query/set what a running command status should be.
+
     Attributes:
         current_status:
-        related_command_event:
-        client:
+        related_command_event: The MessageEvent containing data about the original event used to start the command
+        client: The MaubotMatrixClient, used to add/cleanup control reactions to the response messages
     """
 
     current_status: ReactionCommandStatus
@@ -49,6 +55,14 @@ class ReactionControlEntry:
         self.reaction_collection_of_event_ids = set()
 
     async def setup(self, pinned_message: EventID) -> None:
+        """
+        Initialize the control reactions so the handler can find them later if needed.
+        Args:
+            pinned_message:
+
+        Returns:
+
+        """
         stop_reaction_event = await self.client.react(
             self.related_command_event.room_id,
             pinned_message,
@@ -64,11 +78,14 @@ class ReactionControlEntry:
             pinned_message,
             ReactionCommandStatus.START.value,
         )
-        self.reaction_collection_of_event_ids.add(stop_reaction_event)
-        self.reaction_collection_of_event_ids.add(pause_reaction_event)
-        self.reaction_collection_of_event_ids.add(start_reaction_event)
+        self.reaction_collection_of_event_ids = {stop_reaction_event, pause_reaction_event, start_reaction_event}
 
     async def cancel(self) -> None:
+        """
+        This will change the status to 'Stop', then redact the reactions placed by the bot as a visual indicator that
+        the command is finished.
+
+        """
         self.stop()
         for reaction_event_id in self.reaction_collection_of_event_ids:
             await self.client.redact(
@@ -88,6 +105,13 @@ class ReactionControlEntry:
         self.current_status = ReactionCommandStatus.PAUSE
 
     async def add_cleanup_control(self, pinned_message: EventID) -> None:
+        """
+        Adds a cleanup reaction to the command response that the reaction handler will be able to redact the command
+        response to save on screen real estate.
+        Args:
+            pinned_message: The EventID of the command response, for finding which reactions to remove
+
+        """
         # Don't actually have to save this, as the handler will be capable of removing old stuff without saving it
         await self.client.react(
             self.related_command_event.room_id,
@@ -100,19 +124,47 @@ class ReactionControlEntry:
 
 
 class TaskSetEntry:
+    """
+    A collection object for holding and grouping Task's.
+
+    Mainly used for cleaning up any running Task coroutines if they need to be stopped, like during a bot restart.
+    """
+
     tasks: List[Task]
 
     def __init__(self) -> None:
         self.tasks = []
 
     def add_tasks(self, new_task: Callable, *args, limit: int = 1) -> None:
+        """
+        Add this Callable and its args 'limit' number of times to the grouping of Tasks.
+
+        Args:
+            new_task: The function name without the ()
+            *args: all the bits that are passed to the function
+            limit: How many copies of this Task should be created. Multiple copies implies either a worker model or a
+                large grouping of one-off requests.
+
+        """
         for _ in range(0, limit):
             self.tasks.append(asyncio.create_task(new_task(*args)))
 
     async def gather_results(self, return_exceptions: bool = False):
+        """
+        If you have elected for your Task to return a result, this will get them as a Tuple
+        Args:
+            return_exceptions: Recommend False for most instances. See asyncio.gather docstring for details
+
+        Returns: Tuple of results collected
+
+        """
         return await asyncio.gather(*self.tasks, return_exceptions=return_exceptions)
 
     async def clear_all_tasks(self) -> None:
+        """
+        Cancel all the tasks in this grouping. Mostly used when finished with a command or on restart to avoid orphans
+
+        """
         for task in self.tasks:
             task.cancel()
         # Use return_exceptions set to True so all tasks actually are finished before exiting the system(or some
@@ -122,9 +174,14 @@ class TaskSetEntry:
 
 class ReactionTaskController:
     """
+    Now you can add reactions to a running command as a controlling mechanism, as well as group any Tasks that may be
+    used for processing the command into a cancellable format.
+
+    Add a ReactionTaskController() to your start() call on your plugin, and call shutdown() in the pre_stop() call.
+
     Attributes:
-        tracked_reactions: Map of EventID of the response of a command that will have the reactions attached to the current
-            Status of the task
+        tracked_reactions: Map of EventID of the response of a command that will have the reactions attached to the
+            current Status of the task
         tasks_sets: Map of Hashable key for reference to a TaskSetEntry of associated Task objects used by the
             command
     """
