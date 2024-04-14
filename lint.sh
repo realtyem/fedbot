96cfa4725ca2427e2a0f7c0726058154cf953c4b
Jason Little <realtyem@gmail.com>
2024-04-14 16:31:33 -0500

    Move removing .mypy_cache to the end of the script

--- lint.sh
+++ lint.sh
@@ -9,12 +9,11 @@ python -m black $@
 echo "Running flake8 on ${@}"
 python -m flake8 $@
 
-echo "Removing .mypy_cache"
-rm -rf .mypy_cache/
-
 echo "Running mypy on ${@}"
 python -m mypy $@
 
 echo "Running pylint on ${@}"
 python -m pylint $@
 
+echo "Removing .mypy_cache"
+rm -rf .mypy_cache/
