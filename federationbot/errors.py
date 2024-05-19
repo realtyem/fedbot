419a28de02d828e47cf3eb7c6418ff597a8bd55f
Jason Little <realtyem@gmail.com>
2024-05-19 10:07:07 -0500

    Re-Superclass ServerDiscoveryError

--- federationbot/errors.py
+++ federationbot/errors.py
@@ -55,7 +55,7 @@ class ServerUnreachable(FedBotException):
     """
 
 
-class ServerDiscoveryError(Exception):
+class ServerDiscoveryError(FedBotException):
     """
     Error occurred during server discovery process
     """
