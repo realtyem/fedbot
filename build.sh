cddfb41b2eef9002eb569648ad487846feef5587
Tom Foster <tom@tcpip.uk>
2025-01-23 17:24:02 +0000

    Make shell scripts safer

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-zip -9r ../fedbot-test.mbp *
+zip -9r ../fedbot-test.mbp ./*
--- lint.sh
+++ lint.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "Running isort on ${@}"
-isort $@
+echo "Running isort on" "$@"
+isort "$@"
 
-echo "Running black on ${@}"
-python -m black $@
+echo "Running black on" "$@"
+python -m black "$@"
 
-echo "Running flake8 on ${@}"
-python -m flake8 $@
+echo "Running flake8 on" "$@"
+python -m flake8 "$@"
 
-echo "Running mypy on ${@}"
-python -m mypy $@
+echo "Running mypy on" "$@"
+python -m mypy "$@"
 
-echo "Running pylint on ${@}"
-python -m pylint $@
+echo "Running pylint on" "$@"
+python -m pylint "$@"
 
 echo "Removing .mypy_cache"
 rm -rf .mypy_cache/
